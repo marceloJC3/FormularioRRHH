c1571390e92551c70d29217147e707d3528b2d8e
Marcelo Mercado Godoy <mmercado@LATINSHARE.COM>
2018-09-27 18:42:44 -0300

    Version Inicial

---
+++ src/webparts/formsDa/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFormsDaWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FormsDaWebPartStrings' {
+  const strings: IFormsDaWebPartStrings;
+  export = strings;
+}
